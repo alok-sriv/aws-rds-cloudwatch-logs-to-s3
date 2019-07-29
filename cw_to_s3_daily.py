7b0a72d11c1a6777507d4dc117e940fdc88c909e
aloks2019 <47266675+aloks2019@users.noreply.github.com>
2019-07-29 19:57:11 -0400

    Create cw_to_s3_daily.py

---
+++ cw_to_s3_daily.py
@@ -0,0 +1,21 @@
+import boto3
+import collections
+from datetime import datetime, timedelta, date, time
+
+region = 'us-east-2'
+def lambda_handler(event, context):
+    log_file = boto3.client('logs')
+    startOfDay = datetime.combine(date.today()-timedelta(1),time())
+    endOfDay = datetime.combine(date.today(),time())
+    unix_start = datetime(1970,1,1)
+    group_name = ['/aws/rds/instance/dbinstancename/postgresql']
+    for x in group_name:
+        response = log_file.create_export_task(
+         taskName='eip_rds_pgsql_export_task',
+         logGroupName=x,
+         fromTime=int((startOfDay-unix_start).total_seconds() * 1000),
+         to=int((endOfDay-unix_start).total_seconds() * 1000),
+         destination='a5hutosh',
+         destinationPrefix='cwlogs/log_date={}/exported_logs-{}'.format(startOfDay.strftime("%Y-%m-%d"),startOfDay.strftime("%Y-%m-%d"))
+         )
+        return 'Response from export task at {} :\n{}'.format(datetime.now().isoformat(),response)
