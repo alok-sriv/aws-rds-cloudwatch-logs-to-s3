701a02c0e3d8a784772c304e93c863288085daa4
aloks2019 <47266675+aloks2019@users.noreply.github.com>
2019-07-29 19:56:02 -0400

    Create cw_to_s3_hourly.py

---
+++ cw_to_s3_hourly.py
@@ -0,0 +1,22 @@
+import boto3
+import collections
+from datetime import datetime, timedelta, date, time
+
+region = 'us-east-2'
+def lambda_handler(event, context):
+    log_file = boto3.client('logs')
+    now=datetime.now()
+    startOfhour = now.replace(minute=0,second=0, microsecond=0)-timedelta(hours=1)
+    endOfhour = now.replace(minute=0,second=0, microsecond=0)
+    unix_start = datetime(1970,1,1)
+    group_name = ['/aws/rds/instance/meals/postgresql']
+    for x in group_name:
+        response = log_file.create_export_task(
+         taskName='eip_rds_pgsql_export_task',
+         logGroupName=x,
+         fromTime=int((startOfhour-unix_start).total_seconds() * 1000),
+         to=int((endOfhour-unix_start).total_seconds() * 1000),
+         destination='a5hutosh',
+         destinationPrefix='cwlogs/log_year={}/log_month={}/log_day={}/log_hour={}/exported_logs-{}'.format(startOfhour.strftime("%Y"),startOfhour.strftime("%b"),startOfhour.strftime("%d"),startOfhour.strftime("%H"),startOfhour.strftime("%Y-%m-%d"))
+         )
+        return 'Response from export task at {} :\n{}'.format(datetime.now().isoformat(),response)
