81bd0b14bc06e8d8d47c66d6eeb3210a6f0423bb
aloks2019 <47266675+aloks2019@users.noreply.github.com>
2019-07-29 20:08:09 -0400

    Update cw_to_s3_hourly.py

--- cw_to_s3_hourly.py
+++ cw_to_s3_hourly.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     group_name = ['/aws/rds/instance/dbinstancename/postgresql']
     for x in group_name:
         response = log_file.create_export_task(
-         taskName='eip_rds_pgsql_export_task',
+         taskName='rds_pgsql_export_task',
          logGroupName=x,
          fromTime=int((startOfhour-unix_start).total_seconds() * 1000),
          to=int((endOfhour-unix_start).total_seconds() * 1000),
