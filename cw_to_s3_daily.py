83de657439028383c2f08d5ce85f5dfcb507f010
aloks2019 <47266675+aloks2019@users.noreply.github.com>
2019-07-29 20:05:09 -0400

    Update cw_to_s3_daily.py

--- cw_to_s3_daily.py
+++ cw_to_s3_daily.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
          logGroupName=x,
          fromTime=int((startOfDay-unix_start).total_seconds() * 1000),
          to=int((endOfDay-unix_start).total_seconds() * 1000),
-         destination='a5hutosh',
+         destination='S3bucketname',
          destinationPrefix='cwlogs/log_date={}/exported_logs-{}'.format(startOfDay.strftime("%Y-%m-%d"),startOfDay.strftime("%Y-%m-%d"))
          )
         return 'Response from export task at {} :\n{}'.format(datetime.now().isoformat(),response)
